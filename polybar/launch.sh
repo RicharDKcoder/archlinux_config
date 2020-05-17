2af17e552cf7b2166ea26ad6586b9e6ebb6208de
docryze <dongkang_magic@aliyun.com>
2020-05-17 22:41:51 +0800

    20-05-1589726511

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -7,7 +7,7 @@ killall -q polybar
 
 # Launch bar1 and bar2
 # echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar example >>/var/log/polybar.log 2>&1 &
+polybar example >>/tmp/polybar.log 2>&1 &
 # polybar bar2 >>/tmp/polybar2.log 2>&1 &
 
 # echo "Bars launched..."
